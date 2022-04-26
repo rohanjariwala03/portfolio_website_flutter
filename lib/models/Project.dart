eb21fb8b98e0e045bbb8b432c5585c3d87a69937
rohanjariwala <rohanjariwala03@gmail.com>
2022-04-26 18:37:33 -0400

    first commit

---
+++ lib/models/Project.dart
@@ -0,0 +1,55 @@
+class Project {
+  final String? title, description;
+  final String url;
+
+  Project({this.title, this.description,required this.url});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "Instagram Clone App Flutter",
+    description:
+    "This app is clone of Instagram in flutter. This app is made with Dart language. This app have following features. Responsive Instagram UI"
+      "Email & Password Authentication"
+  "Share Posts with Caption"
+  "Display Posts with Caption"
+  "Like & Comment on Posts"
+  "Search Users"
+  "Follow Users"
+      "Display User Posts, Followers & Following"
+      "EVERYTHING REAL TIME"
+      "Sign Out",
+    url: 'https://github.com/rohanjariwala03/instagram_clone_flutter'
+
+  ),
+  Project(
+    title: "Food App Flutter",
+    description:
+        "The customer app helps the customer to access the online food ordering platforms, search for the right restaurant or the dish they want to order, place their orders and pay easily. There are many features you can implement in the app to make it easy and exciting to use.",
+    url: 'https://github.com/rohanjariwala03/Food_App_Ui'
+  ),
+  Project(
+    title: "Muscle Monster - Fitness Android App",
+    description:
+    "The purpose of a fitness app is to provide the user with instructions and examples of one or more types of exercise, physical activity, nutritional programs, or some other fitness topic. Many fitness apps are available to download from the internet.",
+    url: 'https://github.com/rohanjariwala03/Muscle-Monster---Fitness-Application'
+  ),
+  Project(
+    title: "Universal Builder - Real Estate Website",
+    description:
+    "Universal Builder is a Real Estate website made with C#. This web have lot of features which should be included in real estate website. Such as this web have mainly 3 parts Client, Builder, Agents/Brokers. And They all can perform their respective tasks such as publish new property, buy property, Auction property, etc.",
+    url: 'https://github.com/rohanjariwala03/Universal_Builders'
+  ),
+  Project(
+    title: "WeatherApp Android",
+    description:
+        "Weather apps enable users to get instant alerts regarding weather conditions. Weather apps are the simplest method to know about the updates of the upcoming weather. This app is made in Android with java for personal projects. ",
+    url: 'https://github.com/rohanjariwala03/WeatherApp-Android'
+  ),
+  Project(
+    title: "Portfolio Website",
+    description:
+    "At its most basic form, a portfolio website provides professional information about an individual or a company and presents a showcase of their work.",
+    url: 'https://github.com/rohanjariwala03/portfolio_website_flutter'
+  ),
+];
---
+++ lib/screens/home/components/home_banner.dart
@@ -0,0 +1,160 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/responsive.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../constants.dart';
+
+class HomeBanner extends StatelessWidget {
+  const HomeBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: Responsive.isMobile(context) ? 2.5 : 3,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/bg.jpeg",
+            fit: BoxFit.cover,
+          ),
+          Container(color: darkColor.withOpacity(0.66)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Discover My Skills",
+                  style: Responsive.isDesktop(context)
+                      ? Theme.of(context).textTheme.headline3!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )
+                      : Theme.of(context).textTheme.headline5!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                ),
+                if (Responsive.isMobileLarge(context))
+                  const SizedBox(height: defaultPadding / 2),
+                MyBuildAnimatedText(),
+                SizedBox(height: defaultPadding),
+                if (!Responsive.isMobileLarge(context))
+                  ElevatedButton(
+                    onPressed: () async{
+                      try{
+                        if (!await launchUrl(Uri.parse("https://github.com/rohanjariwala03"))){
+                        }else{
+                        }
+                      }catch(e){
+                        print("erro " + e.toString());
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: defaultPadding * 2,
+                          vertical: defaultPadding),
+                      backgroundColor: primaryColor,
+                    ),
+                    child: Text(
+                      "EXPLORE NOW",
+                      style: TextStyle(color: darkColor),
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyBuildAnimatedText extends StatelessWidget {
+  const MyBuildAnimatedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      // it applies same style to all the widgets under it
+      style: Theme.of(context).textTheme.subtitle1!,
+      maxLines: 1,
+      child: Row(
+        children: [
+          if (!Responsive.isMobileLarge(context)) FlutterCodedText(),
+          if (!Responsive.isMobileLarge(context))
+            SizedBox(width: defaultPadding / 2),
+          Responsive.isMobile(context)
+              ? Expanded(child: AnimatedText())
+              : AnimatedText(),
+          if (!Responsive.isMobileLarge(context))
+            SizedBox(width: defaultPadding / 2),
+          if (!Responsive.isMobileLarge(context)) FlutterCodedText(),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedText extends StatelessWidget {
+  const AnimatedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedTextKit(
+      animatedTexts: [
+        TyperAnimatedText(
+          "I'm currently working with Flutter",
+          speed: Duration(milliseconds: 60),
+        ),
+        TyperAnimatedText(
+          "I've experience of working with Java.",
+          speed: Duration(milliseconds: 60),
+        ),
+        TyperAnimatedText(
+          "I've experience of working with Kotlin",
+          speed: Duration(milliseconds: 60),
+        ),
+        TyperAnimatedText(
+          "I've experience of working with C#",
+          speed: Duration(milliseconds: 60),
+        ),
+        TyperAnimatedText(
+          "I've experience of working with C++",
+          speed: Duration(milliseconds: 60),
+        ),
+      ],
+    );
+  }
+}
+
+class FlutterCodedText extends StatelessWidget {
+  const FlutterCodedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        text: "<",
+        children: [
+          TextSpan(
+            text: "flutter",
+            style: TextStyle(color: primaryColor),
+          ),
+          TextSpan(text: ">"),
+        ],
+      ),
+    );
+  }
+}
